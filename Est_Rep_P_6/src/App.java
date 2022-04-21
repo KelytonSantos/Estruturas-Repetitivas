34e45ab47e03ae1e0f826f7e77cee4ba4754a9bc
Kelyton Santos <kelytonlucas@gmail.com>
2022-04-21 13:57:53 -0300

    Adicionado estrutura if else

--- Est_Rep_P_6/src/App.java
+++ Est_Rep_P_6/src/App.java
@@ -3,21 +3,34 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+//---------------------------------------------------------------------------------------------------------------------
 
-        int senha;
+        System.out.println("Atividade 2:");
+        Scanner sc1 = new Scanner (System.in);
 
-        System.out.println("Digite a senha: ");
-        senha = sc.nextInt();
-        
-        while (senha != 2002) {
-            System.out.println("Senha Invalida.\n\n\n");
-            System.out.println("Digite a senha: ");
-            senha = sc.nextInt();
+        System.out.println("Digite o valor da coordenada x: ");
+        int x = sc1.nextInt();
+        System.out.println("Digite o valor da coordenada y: ");
+        int y = sc1.nextInt();
 
+        while (x != 0 && y != 0){
+           if(x > 0 && y > 0){
+                System.out.println("Primeiro quadrante");
+           }
+           else if(x < 0 && y < 0){
+                System.out.println("Terceiro quadrante");
+           }
+           else if(x > 0 && y < 0){
+                System.out.println("Quarto quadrante");
+           }
+           else if(x < 0 && y > 0){
+                System.out.println("Segundo quadrante");
+           }
+
+            x = sc1.nextInt();
+            y = sc1.nextInt();
         }
-        System.out.println("Acesso Permitido");
 
-        sc.close();
+        sc1.close();
     }
 }
